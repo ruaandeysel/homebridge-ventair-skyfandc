188f18f7564b39bca0575a7b5abcd45e2da831a4
MrD3y5eL <57021511+domalab@users.noreply.github.com>
2024-07-04 12:28:20 +1000

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgeSkyfanDCCeilingFan';
+export const PLATFORM_NAME = 'HomebridgeVentairCeilingFan';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-skyfandc-ceiling-fan';
+export const PLUGIN_NAME = 'homebridge-ventair-ceiling-fan';
